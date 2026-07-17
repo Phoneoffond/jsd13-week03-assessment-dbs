46750618e78e214a5bd15e680c69a9067c5cfeb2
fon.suteara <fon.suteara@gmail.com>
2026-07-17 16:43:32 +0700

    Answer bonus postgresql task 1

--- 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
+++ 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
@@ -16,5 +16,11 @@
 // are involved, and what MongoDB concepts you plan to use.
 // Write in English or Thai. Do not skip this step.
 //
-// Your thinking:
-//
+// Your thinking: ดึง side หรือเครื่องเคียงมาจากฐานข่้อมูล menu_items ภายใต้หมวดหมู้ category ที่ตรงกัน
+// จากนั้นไม่ให้แสดงผล _id แต่ให้แสดงผลชื่อ name และราคา price
+use("chrome-burger-db");
+
+db.menu_items.find( 
+    {"category" : "Side"},
+    {_id: false, name: true, price: true}
+);
