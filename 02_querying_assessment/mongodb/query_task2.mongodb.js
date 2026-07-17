c5066142941dcc4db1835fc6caadf766f9994b1a
fon.suteara <fon.suteara@gmail.com>
2026-07-17 13:40:22 +0700

    Answer mongodb task 2

--- 02_querying_assessment/mongodb/query_task2.mongodb.js
+++ 02_querying_assessment/mongodb/query_task2.mongodb.js
@@ -17,5 +17,14 @@
 // are involved, and what MongoDB concepts you plan to use.
 // Write in English or Thai. Do not skip this step.
 //
-// Your thinking:
-//
+// Your thinking: เจนต้องการดึงชื่อออเดอร์กับราคามาให้ผู้จัดการดู โดยไม่ใส่รายละเอียดอย่างอื่น 
+// ตอนแรกให้ดึงข้อมูลมาจากฐานข้อมูลที่มีชื่อว่า staff โดยใช้ find() หาชื่อ Jane และ นามสกุล Doe
+// ใส่เงื่อนไขว่าจะดูเฉพาะ order_date (วันที่) และ total_price (ราคาสุทธิ) ด้วยค่า true
+// ตอนรันโค้ดมี _id ขึ้นมาด้วย เลยแก้ด้วยการใส่ false เพื่อไม่แสดงผล _id
+
+use("chrome-burger-db");
+
+
+db.orders.find( {"staff.first_name" : "Jane", "staff.last_name" : "Doe"},
+    {"_id": false, "order_date": true, "total_price": true}
+);
